053702f50a62a91668a9f77274413e8a759a3875
rajnishwise <70053522+rjrajnish@users.noreply.github.com>
2024-05-25 10:43:04 +0530

    update protfolio

---
+++ README.md
@@ -0,0 +1,44 @@
+<div align="center">
+  
+ 
+
+  <h2 align="center">Rajnish Pandey - Personal portfolio</h2>
+
+This website is fully responsive personal portfolio, <br />Responsive for all devices, built using Nextjs, TailwindCSS only.
+
+<!-- <a href="https://osamajavaid.vercel.app/"><strong>➥ Live Demo</strong></a> -->
+
+</div>
+
+<br />
+
+ 
+
+ 
+
+### Prerequisites
+
+Before you begin, ensure you have met the following requirements:
+
+- [Git](https://git-scm.com/downloads "Download Git") must be installed on your operating system.
+
+### Run Locally
+
+To run **portfolio** locally, run this command on your git bash:
+
+Linux and macOS:
+
+```bash
+sudo git clone https://github.com/osamajavaid/portfolio.git
+```
+
+Windows:
+
+```bash
+git clone https://github.com/osamajavaid/portfolio.git
+```
+
+ 
+### License
+
+This project is **free to use** and does not contains any license.
---
+++ constants/constants.js
@@ -0,0 +1,78 @@
+export const NAME = 'Rajnish Pandey';
+
+export const DESIGNATION = `MERN Full-Stack Developer | React Developer | NodeJs Developer | Python Developer | GoLang Developer`;
+
+export const DETAILS = {
+  Residence: 'UP, India',
+  City: 'Noida',
+  Age: '25',
+};
+
+export const SKILLS = [
+  {
+    title: 'MERN Stack Developer',
+    level: '80%',
+  },
+  {
+    title: 'React Developer',
+    level: '91%',
+  },
+  {
+    title: 'Frontend Developer',
+    level: '70%',
+  },
+  {
+    title: 'Backend Developer',
+    level: '75%',
+  },
+
+];
+
+export const TECH_STACK = [
+  'JavaScript',
+  'TypeScript',
+  'Python',
+  'Django',
+  'Tornado',
+  'ReactJS',
+  'NextJS',
+  'NodeJS',
+  'Redux',
+  'NestJs',
+  'ExpressJs',
+  'GoLang',
+  'CSS',
+  'SCSS',
+  'TailwindCSS',
+  'Ant Design',
+  'Material UI',
+  'Next UI',
+  'REST API',
+  'MongoDB',
+  'MySQL',
+  'MsSql',
+  'Filezilla',
+  'Ubuntu',
+  'Linux',
+  'Git',
+  'GitHub',
+  'Jira',
+  'Bitbucket',
+  'Trello',
+  'Flutter',
+  'AWS',
+  'Hosting',
+  'DevOps',
+  'Agile'
+];
+
+export const CONTACTS = {
+  EMAIL: 'rajnishpandey010@gmail.com',
+  PHONE: '+91-7985574363',
+};
+export const SOCIAL_LINKS = {
+  GITHUB: 'https://github.com/rjrajnish',
+  LINKEDIN: 'https://www.linkedin.com/in/rajnish-pandey-21a47a1a7/',
+  TWITTER: 'https://twitter.com/rjrajnish9090',
+  FACEBOOK: 'https://www.facebook.com/rjpandeyrajnish',
+};
